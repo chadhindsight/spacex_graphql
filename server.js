54c7e2916729d47dd63cff4eca7f5794fb661a6d
Chad Hinds <chadrickj8@gmail.com>
2020-06-15 21:15:28 -0400

    Created Graphql server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema')
+const app = express();
+// Initialize express with graphql. Only need one endpoint
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+    })
+);
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
