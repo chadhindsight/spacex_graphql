c699d104c9efe02d0ed49a260ad1cb88a150e04d
Chad Hinds <chadrickj8@gmail.com>
2020-06-17 14:06:16 -0400

    let us try again

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use('/graphql', graphqlHTTP({
 );
 app.use(express.static('public'));
 
-app.get("*", (req,res) =>{
+app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-})
+});
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
